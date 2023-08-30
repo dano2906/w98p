3455b5dcbdeea50b027f6b4fd25f048389168953
Dano <dlg9806@gmail.com>
2023-08-30 12:46:09 +0200

    feat: Shortcut desktop and projects windows done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,11 +7,17 @@ module.exports = {
           greenBody: '#008080',
           grayMedium: '#7f7f7f',
           grayLight: '#c0c0c0',
+          grayLighter: '#c6c6c6',
           grayDark: '#4e4e4e',
-          grayDarker: '#404040'
+          grayDarker: '#404040',
+          blueDark: '#000080',
+          blueLight: '#0000EE'
         },
         fontFamily: {
           levi: ['Levi', 'sans-serif']
+        },
+        borderWidth: {
+          '1': '1px'
         }
       },
     },
