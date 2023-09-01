7d7b053723cb288bf4d76451d9f174b2ef23ae16
Dano <dlg9806@gmail.com>
2023-09-01 15:07:03 +0200

    upgrade: Astro v3 and AstroTailwind v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  experimental: {
-   assets: true
-  },
   integrations: [
     tailwind({
       configFile: './tailwind.config.cjs',
